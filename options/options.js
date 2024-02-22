6ec2a3e9136777203c2d6a0aebcae4540eadb3e3
Jordan Heale <webmaster@pridit.co.uk>
2024-02-22 17:24:03 +0000

    feat: add support for numeric sorting, options page, basic eslint,  bump manifest version

---
+++ options/options.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  const checkboxes = document.getElementsByClassName('option');
+  const options = {};
+
+  let element;
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    element = checkboxes[i];
+
+    options[element.dataset.option] = element;
+
+    element.addEventListener('change', function(e) {
+      if (!e.isTrusted) return;
+
+      const store = {};
+
+      store[this.dataset.option] = this.checked;
+
+      browser.storage.sync.set(store);
+    });
+  }
+
+  browser.storage.sync.get(Object.keys(options)).then((items) => {
+    for (const item in items) {
+      element = options[item];
+      element.checked = items[item];
+    }
+  });
+
+  browser.storage.sync.onChanged.addListener((changes) => {
+    const changedItems = Object.keys(changes);
+
+    for (const item of changedItems) {
+      if (options[item]) {
+        options[item].checked = !!changes[item].newValue;
+      }
+    }
+  });
+}());
--- sort.js
+++ sort.js
@@ -1,69 +1,76 @@
-// move playlists outside of the expandable list, into the list, so it can all be sorted
-wait("#section-items").then((item) => {
-  onVisible(document.querySelector(".ytd-guide-entry-renderer"), () => shuffle());
-});
-
-const elements = [
-  ["expandable-items", "#expandable-items"],
-  ["playlists", ".ytd-add-to-playlist-renderer"]
-];
-
-elements.forEach(function(element) {
-  wait(element[1]).then((item) => {
-    onVisible(document.querySelector(element[1]), () => sort(element[0]));
+browser.storage.sync.get({
+  'yps-1-ll': true,
+  'yps-2-wl': false
+}, function (items) {
+  // move playlists outside of the expandable list, into the list, so it can all be sorted
+  wait("#section-items").then(() => {
+    onVisible(document.querySelector(".ytd-guide-entry-renderer"), () => shuffle());
   });
-});
 
-function shuffle() {
-  let section = document.getElementById("section-items");
+  const elements = [
+    ["expandable-items", "#expandable-items"],
+    ["playlists", ".ytd-add-to-playlist-renderer"],
+  ];
 
-  [...section.children]
-    .forEach((item) => {
-      let href = item.querySelector('a').href;
-
-      if (href.indexOf('/playlist') > 0 && href.length > 40) {
-        document.getElementById("expandable-items").appendChild(item);
-      }
-  });
-}
-
-function sort(element) {
-  let playlists = document.getElementById(element);
-
-  [...playlists.children]
-    .sort((a,b)=>a.innerText.toLowerCase()>b.innerText.toLowerCase()?1:-1)
-    .forEach((node) => {
-      if (node.innerText === "Watch Later") return;
-      playlists.appendChild(node)
+  elements.forEach(function (element) {
+    wait(element[1]).then(() => {    
+      onVisible(document.querySelector(element[1]), () => sort(element[0]));
     });
-}
+  });
 
-function wait(selector) {
-  return new Promise(resolve => {
-    if (document.querySelector(selector)) {
-      return resolve(document.querySelector(selector));
-    }
+  function shuffle() {
+    let section = document.getElementById("section-items");
 
-    const observer = new MutationObserver(mutations => {
+    [...section.children]
+      .forEach((item) => {
+        let href = item.querySelector('a').href;
+
+        if (href.indexOf('/playlist') > 0 && href.length > 40) {
+          document.getElementById("expandable-items").appendChild(item);
+        }
+    });
+  }
+
+  function sort(element) {
+    let playlists = document.getElementById(element);
+
+    [...playlists.children]
+      .sort((a,b) => a.innerText.localeCompare(b.innerText, undefined, {numeric: true}))
+      .forEach((node) => {
+        if (!items['yps-1-ll'] && node.innerText === "Liked videos") return;
+        if (!items['yps-2-wl'] && node.innerText === "Watch Later") return;
+
+        playlists.appendChild(node)
+      });
+  }
+
+  function wait(selector) {
+    return new Promise(resolve => {
       if (document.querySelector(selector)) {
-        resolve(document.querySelector(selector));
-        observer.disconnect();
+        return resolve(document.querySelector(selector));
       }
-    });
 
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true
-    });
-  });
-}
+      const observer = new MutationObserver(() => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
 
-function onVisible(element, callback) {
-  new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        callback(element);
-      }
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
     });
-  }).observe(element);
-}
+  }
+
+  function onVisible(element, callback) {
+    new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          callback(element);
+        }
+      });
+    }).observe(element);
+  }
+});
